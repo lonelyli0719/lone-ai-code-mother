eaf52a4b9fdb13c6e3394d32d696dd6088d85be9
lonelyli0719 <794156416@qq.com>
2025-08-23 23:40:07 +0800

    feat(core): 实现 AI 代码生成和保存功能
    - 新增 HtmlCodeResult 和 MultiFileCodeResult 类用于存储生成的代码结果- 创建 AiCodeGeneratorService 接口定义生成代码的方法
    - 实现 AiCodeGeneratorServiceFactory 用于创建 AI 代码生成服务
    - 添加 CodeParser 接口和具体实现类用于解析生成的代码- 实现 CodeFileSaverTemplate 和具体模板类用于保存代码到文件
    - 创建 AiCodeGeneratorFacade 类封装生成和保存代码的逻辑
    - 添加单元测试用例验证代码生成和保存功能
    - 门面模式、执行器模式、模板方法、策略方法优化代码

---
+++ src/main/java/com/lone/loneaicodemother/ai/AiCodeGeneratorServiceFactory.java
@@ -0,0 +1,38 @@
+package com.lone.loneaicodemother.ai;
+
+import dev.langchain4j.model.chat.ChatModel;
+import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.service.AiServices;
+import jakarta.annotation.Resource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * ai 服务创建工厂
+ * @author lone
+ */
+@Configuration
+public class AiCodeGeneratorServiceFactory {
+
+    @Resource
+    private ChatModel chatModel;
+
+    @Resource
+    private StreamingChatModel streamingChatModel;
+
+    /**
+     * 创建ai代码生成器服务
+     */
+//    @Bean
+//    public AiCodeGeneratorService aiCodeGeneratorService() {
+//        return AiServices.create(AiCodeGeneratorService.class, chatModel);
+//    }
+    @Bean
+    public AiCodeGeneratorService aiCodeGeneratorService() {
+        return AiServices.builder(AiCodeGeneratorService.class)
+                .chatModel(chatModel)
+                .streamingChatModel(streamingChatModel)
+                .build();
+    }
+
+}
