81555ab3462e282c41029f3f7d71597ca18f56c5
lonelyli0719 <794156416@qq.com>
2025-08-23 14:53:21 +0800

    feat: 初始化用户模块
    - 创建用户相关表结构
    - 实现用户注册、登录、登出功能
    - 添加用户信息查询接口- 集成 MyBatis Flex 代码生成器
    - 添加数据库连接配置

--- src/main/java/com/lone/loneaicodemother/LoneAiCodeMotherApplication.java
+++ src/main/java/com/lone/loneaicodemother/LoneAiCodeMotherApplication.java
@@ -1,11 +1,13 @@
 package com.lone.loneaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
+@MapperScan("com.lone.loneaicodemother.mapper")
 public class LoneAiCodeMotherApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/lone/loneaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.lone.loneaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.lone.loneaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author Lone
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
