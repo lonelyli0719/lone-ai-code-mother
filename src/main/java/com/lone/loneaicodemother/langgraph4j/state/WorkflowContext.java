2a6d7edb9b9f239354e1bc31837215338937beb7
lonelyli0719 <794156416@qq.com>
2025-09-07 23:30:21 +0800

    feat(langgraph4j): 实现并发图片收集功能
    - 新增多个图片收集节点，包括内容图片、插画、架构图和 Logo
    - 实现图片收集计划生成和执行并发工作流
    - 添加中间结果存储和最终图片聚合逻辑
    - 优化工作流执行配置，支持并发执行

--- src/main/java/com/lone/loneaicodemother/langgraph4j/state/WorkflowContext.java
+++ src/main/java/com/lone/loneaicodemother/langgraph4j/state/WorkflowContext.java
@@ -1,5 +1,6 @@
 package com.lone.loneaicodemother.langgraph4j.state;
 
+import com.lone.loneaicodemother.langgraph4j.model.ImageCollectionPlan;
 import com.lone.loneaicodemother.langgraph4j.model.ImageResource;
 import com.lone.loneaicodemother.langgraph4j.model.QualityResult;
 import com.lone.loneaicodemother.model.enums.CodeGenTypeEnum;
@@ -77,6 +78,20 @@ public class WorkflowContext implements Serializable {
      * 质量检查结果
      */
     private QualityResult qualityResult;
+
+    /**
+     * 图片收集计划
+     */
+    private ImageCollectionPlan imageCollectionPlan;
+
+
+    /**
+     * 并发图片收集的中间结果字段
+     */
+    private List<ImageResource> contentImages;
+    private List<ImageResource> illustrations;
+    private List<ImageResource> diagrams;
+    private List<ImageResource> logos;
 
 
     @Serial
