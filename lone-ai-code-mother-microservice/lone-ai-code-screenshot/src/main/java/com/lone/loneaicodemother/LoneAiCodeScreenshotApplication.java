6281b5bf07cd873a3f0ac83113a307ae548491ca
lonelyli0719 <794156416@qq.com>
2025-09-13 18:55:48 +0800

    微服务改造 - 截图服务

---
+++ lone-ai-code-mother-microservice/lone-ai-code-screenshot/src/main/java/com/lone/loneaicodemother/LoneAiCodeScreenshotApplication.java
@@ -0,0 +1,12 @@
+package com.lone.loneaicodemother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoneAiCodeScreenshotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoneAiCodeScreenshotApplication.class, args);
+    }
+}
+
